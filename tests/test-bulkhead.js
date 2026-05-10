a223e06c1ef002d7e7acdfc62d8e179341e779d4
Ioan Herbil <ionut.herbil@gmail.com>
2026-05-10 12:58:27 +0300

    test: add bulkhead concurrency test

---
+++ tests/test-bulkhead.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+function makeRequest() {S
+  return new Promise((resolve) => {
+    http.get("http://localhost:3001/movies", (res) => {
+      let data = "";
+      res.on("data", (chunk) => (data += chunk));
+      res.on("end", () => resolve(JSON.parse(data)));
+    });
+  });
+}
+
+Promise.all(Array.from({ length: 6 }, makeRequest)).then((results) => {
+  results.forEach((r, i) => console.log(`Request ${i + 1}: ${r.source}`));
+});
